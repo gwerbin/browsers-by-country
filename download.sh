817eedac3fa32a3ba82463f3d6d8f30bfdc43276
Gregory Werbin <ourobourbon@gmail.com>
2015-08-14 16:18:18 -0400

    version 2

--- download.sh
+++ download.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
-while IFS=$'\t' read iso country; do
-  country=$(echo $country | sed 's/ /%20/g')
-  file="${iso}-${country}.csv"
-  url='http://gs.statcounter.com/chart.php?device=Desktop%2C%20Tablet%20%26%20Console&device_hidden=desktop%2Btablet%2Bconsole&statType_hidden=browser&region_hidden='${iso}'&granularity=yearly&statType=Browser&region='${country}'&fromInt=2008&toInt=2015&fromYear=2008&toYear=2015&multi-device=true&csv=1'
-  echo $url
-  # if [[ ! -f $file ]]; then
-  #   echo "${country}: saving to ${file}" 
-  #   # curl -o $file $url
-  # fi
-  # if [[ -f $file && -z $(head -n 1 $file | grep 'Country') ]]; then
-  #   sed -i ".bak" "1! s/^/${iso},/;1 s/^/\"Country\",/" $file
-  #   rm ${file}.bak
-  # fi
-done < <(head iso2.txt)
+if [[ ! -d data ]]; then mkdir data; fi
+
+for year in 20{08..15}; do
+  case "$year" in
+    2008) months=$(seq -s ' ' 7 12);;
+    2015) months=$(seq -s ' ' 1 7);;
+    *) months=$(seq -s ' ' 1 12);;
+  esac
+  for month in ${months}; do
+    echo "Saving data from ${month}, ${year} to data/${month}-${year}.csv"
+    # curl -L -o "data/${month}-${year}.csv" "http://gs.statcounter.com/download/browser-country/?year=${year}&month=${month}"
+  done
+done
 
